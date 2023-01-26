d54ff57c07fca32f603614eaa6ca910907142e1c
mtwebworks <mt.webworks@gmail.com>
2023-01-26 22:00:29 +0100

    feat: add index.tsx, App.tsx, App.css, logo.svg files

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import LOGO from './logo.svg'
+
+export const App = () => {
+  return (
+    <>
+      <div>
+        <img className="logo" src={LOGO} alt="react logo" />
+        <h1>React TypeScript Boilerplate</h1>
+        <p>
+          - {process.env.NODE_ENV} {process.env.name} -
+        </p>
+      </div>
+    </>
+  )
+}
